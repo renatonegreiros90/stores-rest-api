2d05b0faae518d1052ceb69d7b0a8eb1479461ab
renato <renatonegreiros90@gmail.com>
2021-04-03 14:00:27 -0300

    New try to fix the register bug.

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(('DATABASE_URL').replace("://", "ql://", 1), 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
@@ -27,6 +28,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
-db.init_app(app)
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
